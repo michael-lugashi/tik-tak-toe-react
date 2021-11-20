c9c2487ea276d2a95b12ff3878aeb9f94143b94e
michael-lugashi <michael.lugashi@gmail.com>
2021-11-20 19:51:31 +0200

    set up files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
